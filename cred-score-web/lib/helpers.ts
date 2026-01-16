5f0db28114834950cc959473ce85a457b447ede8
Gaurav <burandegaurav8899@gmail.com>
2026-01-16 19:50:46 +0530

    feat: get cred score transaction builder helper

---
+++ cred-score-web/lib/helpers.ts
@@ -0,0 +1,61 @@
+import {
+  getInitializeInstructionAsync,
+  getChatWithLlmInstructionAsync,
+} from "@/program-client";
+import {
+  address,
+  Address,
+  appendTransactionMessageInstructions,
+  createNoopSigner,
+  createSolanaRpc,
+  createTransactionMessage,
+  getAddressEncoder,
+  getProgramDerivedAddress,
+  pipe,
+  setTransactionMessageFeePayerSigner,
+  setTransactionMessageLifetimeUsingBlockhash,
+} from "@solana/kit";
+export const getCredScoreTransaction = async (
+  twitter_context: string,
+  seed = 0,
+  user: Address
+) => {
+  const rpc = createSolanaRpc("https://api.devnet.solana.com");
+  const addressEncoder = getAddressEncoder();
+  const llmProgramAddress = address(
+    "LLM4VF4uxgbcrUdwF9rBh7MUEypURp8FurEdZLhZqed"
+  );
+  const addressBytes = addressEncoder.encode(user);
+  const [chatContext] = await getProgramDerivedAddress({
+    seeds: [Buffer.from("chat_context"), addressBytes, Buffer.from([seed])],
+    programAddress: llmProgramAddress,
+  });
+  const [inference] = await getProgramDerivedAddress({
+    seeds: [
+      Buffer.from("inference"),
+      addressBytes,
+      addressEncoder.encode(chatContext),
+    ],
+    programAddress: llmProgramAddress,
+  });
+  const signer = createNoopSigner(user);
+  const initIxn = await getInitializeInstructionAsync({
+    chatContext,
+    seed,
+    signer,
+  });
+  const chatWithLLmIxn = await getChatWithLlmInstructionAsync({
+    user: signer,
+    text: twitter_context,
+    chatContext,
+    inference,
+  });
+  const { value: blockhash } = await rpc.getLatestBlockhash().send();
+  const transactionMessage = pipe(
+    createTransactionMessage({ version: 0 }),
+    (tx) => setTransactionMessageFeePayerSigner(signer, tx),
+    (tx) => setTransactionMessageLifetimeUsingBlockhash(blockhash, tx),
+    (tx) => appendTransactionMessageInstructions([initIxn, chatWithLLmIxn], tx)
+  );
+  return transactionMessage;
+};
